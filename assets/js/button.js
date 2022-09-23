9cbeaf4056d66905e4d2dd64d45a849047ac718e
Igor Lopes <claudioigorai@gmail.com>
2022-09-23 15:32:23 -0300

    second commit

--- assets/js/button.js
+++ assets/js/button.js
@@ -10,3 +10,24 @@ $(function(){
     })
 
 });
+
+
+$('.nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
+
+  $('.menu a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
