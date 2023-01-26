6b57f6facf55f1b1e7ef61dde0d4994d6914a962
igorlops <claudioigorai@gmail.com>
2023-01-26 12:02:11 -0300

    Mudanças no header

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,20 +1,40 @@
-const welcome = document.querySelector('.welcome h2');
-typeWrite(welcome);
+var div = document.querySelector('.welcome h1');
+var textos = ['Seja bem vindo ao meu portfólio pessoal', 'Eu sou o Igor', 'Estou estudando desenvolvimento web e sistemas'];
 
-
-
-function typeWrite(element){
-    const textoArray = element.innerHTML.split('');
-    element.innerHTML = '';
-
-    textoArray.forEach((letra,i) =>{
-        setTimeout(()=>{
-            element.innerHTML += letra;
-        }, 75*i);
-
-})
-
-    
-
+function escrever(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 500); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 100);
 }
 
+function limpar(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 100);
+}
+
+function rodape(conteudos, el) {
+    var atual = -1;
+    function prox(cb){
+        if (atual < conteudos.length - 1) atual++;
+        else atual = 0;
+        var str = conteudos[atual];
+        escrever(str, function(){
+            limpar(prox);
+        });
+    }
+    prox(prox);
+}
+rodape(textos);
